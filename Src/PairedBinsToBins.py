a7119056b983738955a12ef536a480bfd0be495b
hollyritch <95076937+hollyritch@users.noreply.github.com>
2022-05-09 15:16:20 +0200

    Add files via upload

--- Src/PairedBinsToBins.py
+++ Src/PairedBinsToBins.py
@@ -14,27 +14,56 @@ def getBins(Bins:list):
 def fromPairedBinsToBins(pairedBins, Bins, graph, Exons:list):
     BinT = namedtuple('BinT', 'exons count')
     # Lies Bins aus PairedBins
+    counter = 0
     for bin in pairedBins: 
+        if counter == 0:
+            addedBins = []
+            counter = 1
+        real_last_left_exon = bin.leftExons[len(bin.leftExons)-1]
+        real_first_right_exon = bin.rightExons[0]
+
         last_left_exon = bin.leftExons[len(bin.leftExons)-1]
         first_right_exon = bin.rightExons[0]
+        while(last_left_exon >= first_right_exon and (len(bin.leftExons)>1 or len(bin.rightExons)>1)):
+            if len(bin.leftExons)>1:
+                bin.leftExons.remove(bin.leftExons[len(bin.leftExons)-1])
+                last_left_exon = bin.leftExons[len(bin.leftExons)-1]
+            else:
+                if len(bin.rightExons)>1:
+                    bin.rightExons.remove(bin.rightExons[0])
+                    first_right_exon = bin.rightExons[0]
+        exonBin = bin.leftExons + bin.rightExons
+        
 
-        if (len(bin.leftExons)+ len(bin.rightExons)==2 and int(last_left_exon) + 1 == int(first_right_exon)):
-            break
-        if first_right_exon >= last_left_exon:
-            
-            startNodeLastLeftExon = last_left_exon + 2
-            endNodeFirstRightExon = len(Exons)*2+1-first_right_exon 
+        # if (len(bin.leftExons) + len(bin.rightExons)==2 and int(last_left_exon) + 1 == int(first_right_exon)):
+        #     break 
+        # if last_left_exon == first_right_exon:
+        #     if len(bin.leftExons)>=len(bin.rightExons):
+        #         exons = bin.leftExons + bin.rightExons[0:len(bin.rightExons)-2]
+
+        # if first_right_exon >= last_left_exon:
+        if exonBin not in addedBins and real_last_left_exon>=real_first_right_exon:
+            startNodeLastLeftExon = real_last_left_exon + 2
+            endNodeFirstRightExon = len(Exons)*2+1-real_first_right_exon 
             full_path_dict = PairedBinToBinEnumeration(str(startNodeLastLeftExon), str(endNodeFirstRightExon), [str(startNodeLastLeftExon)], {}, [0], graph)
             if full_path_dict != None:
                 for key, value in full_path_dict.items():
-                    newBin = bin.leftExons + value + bin.rightExons
+                    if len(value)>2:
+                        newBin = bin.leftExons + value[1:len(value)-2] + bin.rightExons
+                    else:
+                        newBin = bin.leftExons + bin.rightExons
                     newBinBoolean = True
                     for bin1 in Bins:
                         if newBin == bin1.exons:
                             newBinBoolean = False
-                            break 
+                            if counter > 1:
+                                break
+                             
+                    # Add all new Bins implied from Paired Bins to Bins
+                    addedBins.append(newBin)
                     if newBinBoolean==True:
                         Bins.append(BinT(exons=newBin, count={}))
+                        
     return Bins
             
 def PairedBinToBinEnumeration (v:str, endNode:str, pfad:list, allpaths:dict, path_number:list, graph):  
