1fe8bd992df691c78cdaadcc8240fbd69ef8c8b5
lauramarie99 <97438732+lauramarie99@users.noreply.github.com>
2022-05-16 08:57:51 +0200

    Update enumeration_paired_bins.py

--- Src/enumeration_paired_bins.py
+++ Src/enumeration_paired_bins.py
@@ -60,7 +60,55 @@ def enumeration_between_nodes(graph,transkripts:list,node:str,path:list,endnode)
             enumeration_between_nodes(graph,transkripts,n,(path + [n]),endnode)
     return transkripts
 
-  
+
+"""
+The restricted version considers multibins
+"""
+def enumeration_between_nodes_restr(graph,transkripts:list,node:str,path:list,endnode,act_bins,bins):
+    
+    if node == endnode:
+        transkripts.append(parse_graph_new.nodepath_to_transcript(graph,path))
+        return
+    elif node == "1":
+        return
+    else:
+        succ = list(graph.adj[node]) # succ contains all successor nodes
+        for n in succ:
+            
+            if graph.edges[node,n]['type'] == "SpliceJunction": # check if there's a splice junction (we would reach a new exon and have to check for compatibility!)
+                start_exon = graph.edges[node,n]["startExon"] # start Exon 
+                end_exon = graph.edges[node,n]["endExon"] # end Exon
+                
+                new_bins = [] # stores all active bins, which contain the start node followed by the end node and at least one more exon 
+                valid = False # checks if there are compatible bins: Active bins, which contain start and end exon, and the start_exon is not the first exon of the bin.
+                
+                for bin1 in act_bins:
+                    for i in range(0,(len(bin1.exons)-1)):
+                        if (start_exon == bin1.exons[i]) and (end_exon == bin1.exons[i+1]):
+                            valid = True
+                            if (i != (len(bin1.exons)-2)):
+                                new_bins.append(bin1)
+
+                # If there are no compatible bins, but other active multi-bins, stop execution of this path
+                if ((valid == False) and (len(act_bins) != 0)):
+                    continue
+
+                # Get all new bins
+                for bin2 in bins:
+                    if ((bin2.exons[0] == start_exon) and (bin2.exons[1] == end_exon)):
+                        if len(bin2.exons) > 2:
+                            new_bins.append(bin2)
+            
+            # If the edge represents helper edge or exon edge, the bin composition is not changed
+            else:
+                new_bins = act_bins
+
+            # Follow the path
+            enumeration_between_nodes_restr(graph,transkripts,n,(path + [n]),endnode,new_bins,bins)          
+            
+    return transkripts
+
+
 # ENUMERATION_PAIREDBINS FUNCTION
 """
 The enumeration_pairedbins function is similar to the enumeration_bins2 function.
