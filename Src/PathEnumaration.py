6e200c8d849abc9dcbc2c6a2fa7a2591d3ad29a0
Thomas Schacker <77416397+TezieG@users.noreply.github.com>
2022-05-26 19:18:48 +0200

    Update PathEnumaration.py

--- Src/PathEnumaration.py
+++ Src/PathEnumaration.py
@@ -48,6 +48,43 @@ def EnuBinConstraint (Graph, Transcript, Path, Node, Bin):
             else:
                  EnuBinConstraint(Graph,Transcript, (Path + [node]), node, Bin)
 
+    return Transcript 
+
+
+#code that acutally works (from laura) 
+def EnuBinConstraint(Graph, Transcript, Path, Node, bins, Bins,NodeNr):
+    if Node == NodeNr:
+        Transcript.append(Parsing.nodepath_to_transcript(Graph, Path))
+        return
+    else:
+        successor = Graph.adj[Node]
+        for node in successor:
+            if Graph.edges[Node, node]['type'] == "SpliceJunction":
+                # print("I AM HERE")
+                StartExon = Graph.edges[Node, node]["startExon"]
+                EndExon = Graph.edges[Node, node]["endExon"]
+                compatible = False
+                copy = []
+                for bin in bins:
+                    print(bin)
+                    for i in range(0, (len(bin.exons) - 1)):
+                        if ((bin.exons[i] == StartExon) and (bin.exons[i + 1] == EndExon)):
+                            compatible = True
+                            if (i != (len(bin.exons) - 2)):
+                                copy.append(bin)
+
+                if (compatible == False and (len(bins) != 0)):
+                    continue
+
+                for bim in Bins:
+                        if ((bim.exons[0] == StartExon) and (bim.exons[1] == EndExon)):
+                                copy.append(bim)
+            else:
+                copy = bins
+
+            EnuBinConstraint(Graph, Transcript, Path + [node], node, copy, Bins,NodeNr)
+
     return Transcript
+
 
 
