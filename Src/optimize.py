f6d14395522f44a3e83fa251f2d1b129833a1e1a
lauramarie99 <lauramarie.pilgram@web.de>
2022-08-17 21:50:26 +0200

    added sparsity constr to optimization function

--- Src/optimize.py
+++ Src/optimize.py
@@ -3,7 +3,7 @@ import gurobipy as gp
 from gurobipy import GRB
 
 
-def model(G_clean, transcripts, norm, sparsity_constr, factor):
+def model(G_clean, transcripts:list, norm, sparsity_constr, factor:int):
     # Extract edge type and counts from graph file into dictionary
     edges_dict = {}
     for edgeKey, edgeValue in G_clean.edges.items():
@@ -39,29 +39,48 @@ def model(G_clean, transcripts, norm, sparsity_constr, factor):
     if sparsity_constr == "L0":
         sparsity_norm0 = model.addVar(name="Sparsity_Constraint_L0") # Sparsity constraint L0 norm
         model.addGenConstrNorm(sparsity_norm0, vars, 0) # Hinzufügen dieser Zeile verändert sofort die Werte der Variablen var1, warum?
-        # model.setObjective((gp.quicksum(helper2[j] for j in edges) + (factor*sparsity_norm0)), GRB.MINIMIZE)
-        model.addConstr(sparsity_norm0 <= factor)
-    elif sparsity_constr == "L1":
-        model.addConstr(vars.sum() <= factor) # Sparsity constraint L1 norm
+        # model.addConstr(sparsity_norm0 <= factor)
+    # elif sparsity_constr == "L1":
+        # model.addConstr(vars.sum() <= factor) # Sparsity constraint L1 norm
 
     if norm == "L1":
+        # Variable
         norm1 = model.addVars(edges, vtype=GRB.CONTINUOUS, name="L1_norm") # L1 norm (Betrag)
+        # Objective function
         for j in edges:
             model.addConstr(helper1[j] == (edges_dict[j] - (gp.quicksum(adj_matrix[i,j] * vars[i] for i in range(len(transcripts))))))
             model.addConstr(norm1[j] >= helper1[j])
             model.addConstr(norm1[j] >= -helper1[j])
-        model.setObjective(gp.quicksum(norm1[j] for j in edges), GRB.MINIMIZE)
+        # Sparsity constraints
+        if sparsity_constr == "L0":
+            model.setObjective((gp.quicksum(norm1[j] for j in edges) + (factor*sparsity_norm0)), GRB.MINIMIZE)
+        elif sparsity_constr == "L1":
+            model.setObjective((gp.quicksum(norm1[j] for j in edges) + (factor*vars.sum())), GRB.MINIMIZE)
+        else:
+            model.setObjective(gp.quicksum(norm1[j] for j in edges), GRB.MINIMIZE)
+
     elif norm == "L0":
+        # Variable
         norm0 = model.addVar(name="L0_norm") # L0 norm
+        # Objective function
         for j in edges:
             model.addConstr(helper1[j] == (edges_dict[j] - (gp.quicksum(adj_matrix[i,j] * vars[i] for i in range(len(transcripts))))))
         model.addGenConstrNorm(norm0, helper1, 0)
-        model.setObjective(norm0, GRB.MINIMIZE)
+        # Sparsity constraints
+        if sparsity_constr == "L0":
+            model.setObjective(norm0 + (factor*sparsity_norm0), GRB.MINIMIZE)
+        elif sparsity_constr == "L1":
+            model.setObjective(norm0 + (factor*vars.sum()), GRB.MINIMIZE)
+        else:
+            model.setObjective(norm0, GRB.MINIMIZE)
+
     elif norm == "L2":
+        # Variables
         norm2_1 = model.addVars(edges, vtype=GRB.CONTINUOUS, name="L2_norm_1") # Absolute value of helper1
         norm2_2 = model.addVars(edges, vtype=GRB.CONTINUOUS, name="L2_norm_2") # Square of norm2_1
         norm2_3 = model.addVar(name="L2_norm_3") # Sum of norm2_2
         norm2_4 = model.addVar(name="L2_norm_4") # Square root of norm2_3 = L2 norm
+        # Objective function
         for j in edges:
             model.addConstr(helper1[j] == (edges_dict[j] - (gp.quicksum(adj_matrix[i,j] * vars[i] for i in range(len(transcripts))))))
             model.addConstr(norm2_1[j] >= helper1[j])
@@ -69,7 +88,13 @@ def model(G_clean, transcripts, norm, sparsity_constr, factor):
             model.addGenConstrPow(norm2_1[j], norm2_2[j], 2)
         model.addConstr(norm2_3 == gp.quicksum(norm2_2[j] for j in edges))
         model.addGenConstrPow(norm2_3, norm2_4, 0.5)
-        model.setObjective(norm2_4, GRB.MINIMIZE)
+        # Sparsity constraints
+        if sparsity_constr == "L0":
+            model.setObjective(norm2_4 + (factor*sparsity_norm0), GRB.MINIMIZE)
+        elif sparsity_constr == "L1":
+            model.setObjective(norm2_4 + (factor*vars.sum()), GRB.MINIMIZE)
+        else:
+            model.setObjective(norm2_4, GRB.MINIMIZE)
 
     model.optimize()
 
