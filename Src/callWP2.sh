52da6590b860d7ef3a71deabb8206a38f8f75b8c
lauramarie99 <lauramarie.pilgram@web.de>
2022-10-02 18:34:49 +0200

    fixed errors, lauras version

--- Src/callWP2.sh
+++ Src/callWP2.sh
@@ -1,105 +1,101 @@
-#!/bin/zsh
+#!/bin/bash
 
 touch ../Results/resultsWP2.csv
-mkdir ../Results/WP2
+datafolder=../Data
 for fileName in human_geuvadis_simulated_5sets human_leukemia_real_5sets
 do
 	mkdir ../Results/WP2/$fileName
-        graphFileName=$fileName".graph"
-        gtfFileName=$fileName".gtf"
-        for mode in full
-        do
+    graphFileName=${fileName}.graph
+    gtfFileName=${fileName}_ref.gtf
+    for mode in full 
+    do
 		mkdir ../Results/WP2/$fileName/$mode
-		for norm in norm0 norm1
+		for norm in norm1 norm0 norm2
 		do
 			mkdir ../Results/WP2/$fileName/$mode/$norm
 			
 			# Write Files for noSparsityConstraint first
-			mkdir ../Results/WP2/$fileName/$mode/$norm/NoSparsityConstraint
-			outputFileName=$fileName$mode$norm"noSparsityConstraint"
-			conda activate TranscriptReconstruction
+			mkdir ../Results/WP2/$fileName/$mode/$norm/NoConstr
+			outputFileName=${fileName}_full_${mode}_${norm}_noconstr_transcripts.gtf
+			conda activate project1
 			echo $fileName $mode $norm 
-			echo python main.py $graphFileName -$mode -opt -$norm -jsonFilename $outputFileName".json" -resultsFilename ../Results/resultsWP2.csv
-                	python main.py $graphFileName -$mode -opt -$norm -jsonFilename $outputFileName".json" -resultsFilename ../Results/resultsWP2.csv
-                	conda deactivate
-               	 	mv $outputFileName".json" ../Results/WP2/$fileName/$mode/$norm/NoSparsityConstraint
-                	conda activate Cufflinks
-                	sh compareGTF_GTF.sh $gtfFileName transcripts.gtf >> ../Results/resultsWP2.csv
-               	 	conda deactivate
-                	mv r.* ../Results/WP2/$fileName/$mode/$norm/NoSparsityConstraint
-                	mv transcripts.gtf ../Results/WP2/$fileName/$mode/$norm/NoSparsityConstraint/$outputFileName"_transcripts.gtf"	
+			echo python main.py ${datafolder}/*/${graphFileName} -$mode -opt -$norm -resultsFilename ../Results/resultsWP2.csv
+            python main.py ${datafolder}/*/${graphFileName} -$mode -opt -$norm -resultsFilename ../Results/resultsWP2.csv
+            conda deactivate
+			conda activate cufflinks
+            sh compareGTF_GTF.sh ${datafolder}/*/${gtfFileName} transcripts.gtf >> ../Results/resultsWP2.csv
+			conda deactivate
+            mv r.* ../Results/WP2/$fileName/$mode/$norm/NoConstr
+            mv transcripts.gtf ../Results/WP2/$fileName/$mode/$norm/NoConstr/${outputFileName}
 		
-			for constraint in constr0 constr1
+			for constraint in constr1 constr0
 			do 
 				mkdir ../Results/WP2/$fileName/$mode/$norm/$constraint
-				for lambda in 0.01 0.05 0.1 0.5 1 2.5 5 10
+				for factor in 0.01 0.1 1 5 10
 				do
-					mkdir ../Results/WP2/$fileName/$mode/$norm/$constraint/$lambda
-					outputFileName=$fileName$mode$norm$constrain$lambda
-                        		conda activate TranscriptReconstruction
-					echo $fileName $mode $norm $constraint $lambda
-					echo python main.py $graphFileName -$mode -opt -$norm -$constraint $lambda -jsonFilename $outputFileName".json" -resultsFilename ../Results/resultsWP2.csv
-					python main.py $graphFileName -$mode -opt -$norm -$constraint $lambda -jsonFilename $outputFileName".json" -resultsFilename ../Results/resultsWP2.csv
-                        		conda deactivate
-                        		mv $outputFileName".json" ../Results/WP2/$fileName/$mode/$norm/$constraint/$lambda
-                        		conda activate Cufflinks
-                        		sh compareGTF_GTF.sh $gtfFileName transcripts.gtf >> ../Results/resultsWP2.csv
-                        		conda deactivate
-                        		mv r.* ../Results/WP2/$fileName/$mode/$norm/$constraint/$lambda
-                        		mv transcripts.gtf ../Results/WP2/$fileName/$mode/$norm/$constraint/$lambda/$outputFileName"_transcripts.gtf"
-                   		done	
+					mkdir ../Results/WP2/$fileName/$mode/$norm/$constraint/$factor
+					outputFileName=${fileName}_${mode}_${norm}_${constraint}_${factor}_transcripts.gtf
+                    conda activate project1
+					echo $fileName $mode $norm $constraint $factor
+					echo python main.py ${datafolder}/*/${graphFileName} -$mode -opt -$norm -$constraint -factor $factor-resultsFilename ../Results/resultsWP2.csv
+					python main.py ${datafolder}/*/${graphFileName} -$mode -opt -$norm -$constraint -factor $factor -resultsFilename ../Results/resultsWP2.csv
+                    conda deactivate
+                    conda activate cufflinks
+                    sh compareGTF_GTF.sh ${datafolder}/*/${gtfFileName} transcripts.gtf >> ../Results/resultsWP2.csv
+                    conda deactivate
+                    mv r.* ../Results/WP2/$fileName/$mode/$norm/$constraint/$factor
+                    mv transcripts.gtf ../Results/WP2/$fileName/$mode/$norm/$constraint/$factor/$outputFileName
+                done	
 			done
 		done	
 	done
 done
 
-gtfFileName=humanDiverse.gtf
+
+gtfFileName=human_diverse_ref.gtf
 for fileName in SRR307903 SRR307911 SRR315323 SRR315334 SRR387661 SRR534291 SRR534307 SRR534319 SRR545695 SRR545723 ERR188021
 do
-        mkdir ../Results/WP2/$fileName
-        graphFileName=$fileName".graph"
-        for mode in full
+    mkdir ../Results/WP2/$fileName
+    graphFileName=${fileName}.graph
+    for mode in full
+    do
+        mkdir ../Results/WP2/$fileName/$mode
+        for norm in norm1 norm0 norm2
         do
-                mkdir ../Results/WP2/$fileName/$mode
-                for norm in norm0 norm1
+            mkdir ../Results/WP2/$fileName/$mode/$norm          
+            # Write Files for noSparsityConstraint first
+            mkdir ../Results/WP2/$fileName/$mode/$norm/NoConstr
+            outputFileName=${fileName}_${mode}_${norm}_noconstr_transcripts.gtf
+            conda activate project1
+            echo $fileName $mode $norm
+			echo python main.py ${datafolder}/*/${graphFileName} -$mode -opt -$norm -resultsFilename ../Results/resultsWP2.csv 
+			python main.py ${datafolder}/*/${graphFileName} -$mode -opt -$norm -resultsFilename ../Results/resultsWP2.csv
+            conda deactivate
+            conda activate cufflinks
+            sh compareGTF_GTF.sh ${datafolder}/*/${gtfFileName} transcripts.gtf >> ../Results/resultsWP2.csv
+            conda deactivate
+            mv r.* ../Results/WP2/$fileName/$mode/$norm/NoConstr
+            mv transcripts.gtf ../Results/WP2/$fileName/$mode/$norm/NoConstr/${outputFileName}
+                        
+            for constraint in constr0 constr1 
+            do      
+                mkdir ../Results/WP2/$fileName/$mode/$norm/$constraint
+                for factor in 0.01 0.1 1 5 10
                 do
-                        mkdir ../Results/WP2/$fileName/$mode/$norm
-                        
-                        # Write Files for noSparsityConstraint first
-                        mkdir ../Results/WP2/$fileName/$mode/$norm/NoSparsityConstraint
-                        outputFileName=$fileName$mode$norm"noSparsityConstraint"
-                        conda activate TranscriptReconstruction
-                        echo $fileName $mode $norm
-			echo python main.py $graphFileName -$mode -opt -$norm -jsonFilename $outputFileName".json" -resultsFilename ../Results/resultsWP2.csv 
-			python main.py $graphFileName -$mode -opt -$norm -jsonFilename $outputFileName".json" -resultsFilename ../Results/resultsWP2.csv
-                        conda deactivate
-                        mv $outputFileName".json" ../Results/WP2/$fileName/$mode/$norm/NoSparsityConstraint
-                        conda activate Cufflinks
-                        sh compareGTF_GTF.sh $gtfFileName transcripts.gtf >> ../Results/resultsWP2.csv
-                        conda deactivate
-                        mv r.* ../Results/WP2/$fileName/$mode/$norm/NoSparsityConstraint
-                        mv transcripts.gtf ../Results/WP2/$fileName/$mode/$norm/NoSparsityConstraint/$outputFileName"_transcripts.gtf"
-                        
-                        for constraint in constr0 constr1 
-                        do      
-                                mkdir ../Results/WP2/$fileName/$mode/$norm/$constraint
-                                for lambda in 0.01 0.05 0.1 0.5 1 2.5 5 10
-                                do
-                                        mkdir ../Results/WP2/$fileName/$mode/$norm/$constraint/$lambda
-                                        outputFileName=$fileName$mode$norm$constrain$lambda
-                                        conda activate TranscriptReconstruction
-					echo $fileName $mode $norm $constraint $lambda
-					echo python main.py $graphFileName -$mode -opt -$norm -$constraint $lambda -jsonFilename $outputFileName".json" -resultsFilename ../Results/resultsWP2.csv
-                                        python main.py $graphFileName -$mode -opt -$norm -$constraint $lambda -jsonFilename $outputFileName".json" -resultsFilename ../Results/resultsWP2.csv
-                                        conda deactivate
-                                        mv $outputFileName".json" ../Results/WP2/$fileName/$mode/$norm/$constraint/$lambda
-                                        conda activate Cufflinks
-                                        sh compareGTF_GTF.sh $gtfFileName transcripts.gtf >> ../Results/resultsWP2.csv
-                                        conda deactivate
-                                        mv r.* ../Results/WP2/$fileName/$mode/$norm/$constraint/$lambda
-                                        mv transcripts.gtf ../Results/WP2/$fileName/$mode/$norm/$constraint/$lambda/$outputFileName"_transcripts.gtf"
-                                done
-                        done
+                    mkdir ../Results/WP2/$fileName/$mode/$norm/$constraint/$factor
+                    outputFileName=${fileName}_${mode}_${norm}_${constraint}_${factor}_transcripts.gtf
+                    conda activate project1
+					echo $fileName $mode $norm $constraint $factor
+					echo python main.py ${datafolder}/*/${graphFileName} -$mode -opt -$norm -$constraint -factor $factor -resultsFilename ../Results/resultsWP2.csv
+                    python main.py ${datafolder}/*/${graphFileName} -$mode -opt -$norm -$constraint -factor $factor -resultsFilename ../Results/resultsWP2.csv
+                    conda deactivate
+                    conda activate cufflinks
+                    sh compareGTF_GTF.sh ${datafolder}/*/${gtfFileName} transcripts.gtf >> ../Results/resultsWP2.csv
+                    conda deactivate
+                    mv r.* ../Results/WP2/$fileName/$mode/$norm/$constraint/$factor
+                    mv transcripts.gtf ../Results/WP2/$fileName/$mode/$norm/$constraint/$factor/$outputFileName
                 done
+            done
         done
+    done
 done
