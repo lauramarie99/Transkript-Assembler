9f167a87eef62d26603b6796148c8cbbdc59839f
lauramarie99 <lauramarie.pilgram@web.de>
2022-10-07 09:02:32 +0200

    changed L2 norm

--- Src/optimize.py
+++ Src/optimize.py
@@ -19,27 +19,27 @@ def cb(model, where):
         model.terminate()
 
 def model(G_clean, transcripts:list, norm, sparsity_constr, factor:int):
+
     # Extract edge type and counts from graph file into dictionary
     edges_dict = {}
     for edgeKey, edgeValue in G_clean.edges.items():
         count = edgeValue["counts"]["c"]
         if edgeValue["type"] == "SpliceJunction" or edgeValue["type"] == "Exon":
             edges_dict[edgeKey] = count
-    # print(edges_dict)
     edges = list(edges_dict.keys())
 
-    # Create Adjazenzmatrix (path,edge): 0/1
-    adj_matrix = {}
+    # Create Adjazenzdictionary (path,edge): 0/1
+    adj_dict = {}
     for i in range(0, len(transcripts)):
         for j in range(1, len(transcripts[i]) - 1):
             startnode = transcripts[i][j]
             endnode = transcripts[i][j + 1]
             current_edge = (startnode, endnode)
             if current_edge in edges:
-                adj_matrix[i, current_edge] = 1
+                adj_dict[i, current_edge] = 1
         for edge in edges:
-            if (i, edge) not in adj_matrix.keys():
-                adj_matrix[i, edge] = 0
+            if (i, edge) not in adj_dict.keys():
+                adj_dict[i, edge] = 0
 
     # Create model for gurobi
     model = gp.Model("Transcript Expression")
@@ -53,17 +53,14 @@ def model(G_clean, transcripts:list, norm, sparsity_constr, factor:int):
     # Optimization
     if sparsity_constr == "L0":
         sparsity_norm0 = model.addVar(name="Sparsity_Constraint_L0") # Sparsity constraint L0 norm
-        model.addGenConstrNorm(sparsity_norm0, vars, 0) # Hinzufügen dieser Zeile verändert sofort die Werte der Variablen var1, warum?
-        # model.addConstr(sparsity_norm0 <= factor)
-    # elif sparsity_constr == "L1":
-        # model.addConstr(vars.sum() <= factor) # Sparsity constraint L1 norm
+        model.addGenConstrNorm(sparsity_norm0, vars, 0)
 
     if norm == "L1":
         # Variable
         norm1 = model.addVars(edges, vtype=GRB.CONTINUOUS, name="L1_norm") # L1 norm (Betrag)
         # Objective function
         for j in edges:
-            model.addConstr(helper1[j] == (edges_dict[j] - (gp.quicksum(adj_matrix[i,j] * vars[i] for i in range(len(transcripts))))))
+            model.addConstr(helper1[j] == (edges_dict[j] - (gp.quicksum(adj_dict[i,j] * vars[i] for i in range(len(transcripts))))))
             model.addConstr(norm1[j] >= helper1[j])
             model.addConstr(norm1[j] >= -helper1[j])
         # Sparsity constraints
@@ -79,7 +76,7 @@ def model(G_clean, transcripts:list, norm, sparsity_constr, factor:int):
         norm0 = model.addVar(name="L0_norm") # L0 norm
         # Objective function
         for j in edges:
-            model.addConstr(helper1[j] == (edges_dict[j] - (gp.quicksum(adj_matrix[i,j] * vars[i] for i in range(len(transcripts))))))
+            model.addConstr(helper1[j] == (edges_dict[j] - (gp.quicksum(adj_dict[i,j] * vars[i] for i in range(len(transcripts))))))
         model.addGenConstrNorm(norm0, helper1, 0)
         # Sparsity constraints
         if sparsity_constr == "L0":
@@ -93,23 +90,23 @@ def model(G_clean, transcripts:list, norm, sparsity_constr, factor:int):
         # Variables
         norm2_1 = model.addVars(edges, vtype=GRB.CONTINUOUS, name="L2_norm_1") # Absolute value of helper1
         norm2_2 = model.addVars(edges, vtype=GRB.CONTINUOUS, name="L2_norm_2") # Square of norm2_1
-        norm2_3 = model.addVar(name="L2_norm_3") # Sum of norm2_2
-        norm2_4 = model.addVar(name="L2_norm_4") # Square root of norm2_3 = L2 norm
+        #norm2_3 = model.addVar(name="L2_norm_3") # Sum of norm2_2
+        #norm2_4 = model.addVar(name="L2_norm_4") # Square root of norm2_3 = L2 norm
         # Objective function
         for j in edges:
-            model.addConstr(helper1[j] == (edges_dict[j] - (gp.quicksum(adj_matrix[i,j] * vars[i] for i in range(len(transcripts))))))
+            model.addConstr(helper1[j] == (edges_dict[j] - (gp.quicksum(adj_dict[i,j] * vars[i] for i in range(len(transcripts))))))
             model.addConstr(norm2_1[j] >= helper1[j])
             model.addConstr(norm2_1[j] >= -helper1[j])
             model.addGenConstrPow(norm2_1[j], norm2_2[j], 2)
-        model.addConstr(norm2_3 == gp.quicksum(norm2_2[j] for j in edges))
-        model.addGenConstrPow(norm2_3, norm2_4, 0.5)
+        #model.addConstr(norm2_3 == gp.quicksum(norm2_2[j] for j in edges))
+        #model.addGenConstrPow(norm2_3, norm2_4, 0.5)
         # Sparsity constraints
         if sparsity_constr == "L0":
-            model.setObjective(norm2_4 + (factor*sparsity_norm0), GRB.MINIMIZE)
+            model.setObjective((gp.quicksum(norm2_2[j] for j in edges) + (factor*sparsity_norm0)), GRB.MINIMIZE)
         elif sparsity_constr == "L1":
-            model.setObjective(norm2_4 + (factor*vars.sum()), GRB.MINIMIZE)
+            model.setObjective((gp.quicksum(norm2_2[j] for j in edges) + (factor*vars.sum())), GRB.MINIMIZE)
         else:
-            model.setObjective(norm2_4, GRB.MINIMIZE)
+            model.setObjective(gp.quicksum(norm2_2[j] for j in edges), GRB.MINIMIZE)
 
     model._cur_obj = float('inf')
     model._time = time.time()
